c29b57709f15e7df4ba1bf52674dcdf5422d8d5d
YousefQwasmeh <151037@ppu.edu.ps>
2020-07-20 00:56:08 +0300

    i worked with part + title parts

--- client/src/Components/HomePage/index.js
+++ client/src/Components/HomePage/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./style.css";
 import LandingPage from "../LandingPage";
 import ContactMe from "../ContactMe";
+import WorkedWith from "../WorkedWith";
 import LatestWorks from "../LatestWorks";
 class HomePage extends React.Component {
   state = {};
@@ -10,7 +11,10 @@ class HomePage extends React.Component {
     return (
       <div className='home-page'>
         <LandingPage />
+        <h1 className='title-part'> &nbsp;&nbsp;&nbsp;&nbsp; Latest Works</h1>
         <LatestWorks />
+        <h1 className='title-part'>&nbsp;&nbsp;&nbsp;&nbsp; I Worked With</h1>
+        <WorkedWith />
         <ContactMe />
       </div>
     );
